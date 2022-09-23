eff27bd55d6a60fee48bedf5cd3bd119a12a7397
yismaw17 <yismawmulaw@yahoo.com>
2022-09-23 05:32:07 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,34 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes most of the words in a string.
- * @s: analized string.
+ * cap_string - capitalizes chars in a string following a separator
  *
- * Return: String with all words capitalized.
+ * @c: character string pointer
+ * Return: char pointer
  */
 char *cap_string(char *c)
 {
-   int i = 0, j,
-       sep[] = {32, '\t', 11,  '\n', 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i = 0, j,
+	    sep[] = {32, '\t', 11,  '\n', 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-   if (c[0] > 96 && c[0] < 123)
-	   c[0] -= 32;
-   while (c[i] != '\0')
-   {
-	   if (c[i] > 96 && c[i] < 123)
-	   {
-		   j = 0;
-		   while (j < 14)
-		   {
-			   if (c[i - 1] == sep[j])
-			   {
-				   c[i] -= 32;
-				   break;
-			   }
-			   j++;
-		   }
-	   }
-	   i++;
-   }
-   return (c);
+	if (c[0] > 96 && c[0] < 123)
+		c[0] -= 32;
+	while (c[i] != '\0')
+	{
+		if (c[i] > 96 && c[i] < 123)
+		{
+			j = 0;
+			while (j < 14)
+			{
+				if (c[i - 1] == sep[j])
+				{
+					c[i] -= 32;
+					break;
+				}
+				j++;
+			}
+		}
+		i++;
+	}
+	return (c);
 }
