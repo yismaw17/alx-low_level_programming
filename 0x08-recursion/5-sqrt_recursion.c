1f7dcf5fe582e1ca8ab786ea8ed4a2367e328add
yismaw17 <yismawmulaw@yahoo.com>
2022-09-28 07:09:01 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -19,11 +19,12 @@ int sqrt2(int a, int b)
 /**
  * _sqrt_recursion - returns the natural square root of n
  * @n: Number Integer
- * 
- * Return: On success 1.
+ *
+ * Return: On success 1
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _sqrt_recursion(int n)
 {
 	return (sqrt2(n, 1));
 }
+
