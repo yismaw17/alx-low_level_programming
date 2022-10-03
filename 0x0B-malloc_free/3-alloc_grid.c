3179c074c1e0eaa001a1ec413e76745785446b75
yismaw17 <yismawmulaw@yahoo.com>
2022-10-03 05:28:22 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,45 +3,45 @@
 #include <stdlib.h>
 
 /**
- *  * alloc_grid - returns a pointer to a 2 dimensional array of integers
- *   * @width: columns
- *    * @height: rows
- *     *
- *      * Return: Double Pointer
- *       */
+ *  alloc_grid - returns a pointer to a 2 dimensional array of integers
+ *  @width: columns
+ *  @height: rows
+ *
+ *  Return: Double Pointer
+ */
 
 int **alloc_grid(int width, int height)
 {
-		int **s;
-			int i, j, k, fr;
+	int **s;
+		int i, j, k, fr;
 
-				fr = 0;
-					if (width <= 0 || height <= 0)
-								return (0);
+		fr = 0;
+			if (width <= 0 || height <= 0)
+						return (0);
 
-						s = malloc(height * sizeof(int *));
-							if (s == 0)
-										return (0);
-								for (i = 0; i < height; i++)
-										{
-													*(s + i) = malloc(width * sizeof(int));
-															if (*(s + i) == 0)
-																		{
-																						fr = 1;
-																									break;
-																											}
-																	for (j = 0; j < width; j++)
-																				{
-																								s[i][j] = 0;
-																										}
-																		}
-									if (fr == 1)
-											{
-														for (k = 0; k <= i; k++)
+			s = malloc(height * sizeof(int *));
+				if (s == 0)
+							return (0);
+					for (i = 0; i < height; i++)
+							{
+										*(s + i) = malloc(width * sizeof(int));
+												if (*(s + i) == 0)
+															{
+																			fr = 1;
+																						break;
+																								}
+														for (j = 0; j < width; j++)
 																	{
-																					free(*(s + k));
+																					s[i][j] = 0;
 																							}
-																free(s);
-																	}
-										return (s);
+															}
+						if (fr == 1)
+								{
+											for (k = 0; k <= i; k++)
+														{
+																		free(*(s + k));
+																				}
+													free(s);
+														}
+							return (s);
 }
