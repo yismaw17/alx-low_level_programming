cc53ca93df7e5a3a0a5470f0da204e743d446e9c
yismaw17 <yismawmulaw@yahoo.com>
2022-10-07 01:09:55 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,43 +2,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *  * _memset -  initializes n bytes of memory to x
- *   * @ptr: initial adress
- *    * @x: variable to initialize with
- *     * @n: number of bytes to initialize
- *      *
- *       * Return: Return pointer char (so movements are 1 byte)
- *        */
+ * _memset -  initializes n bytes of memory to x
+ * @ptr: initial adress
+ * @x: variable to initialize with
+ * @n: number of bytes to initialize
+ *
+ * Return: Return pointer char (so movements are 1 byte)
+ */
 char *_memset(char *ptr, int x, unsigned int n)
 {
-		unsigned int i;
+	unsigned int i;
 
-			for (i = 0; i < n; i++)
-						ptr[i] = x;
+	for (i = 0; i < n; i++)
+		ptr[i] = x;
 
-				return (ptr);
+	return (ptr);
 }
 
 /**
- *  * _calloc -  allocates memory using malloc and initializes in 0
- *   * @nmemb: number of elements of array to allocate
- *    * @size: size of elements
- *     *
- *      * Return: Pointer to allocated memory or normal process termination
- *       * with a status value of 98
- *        */
+ * _calloc -  allocates memory using malloc and initializes in 0
+ * @nmemb: number of elements of array to allocate
+ * @size: size of elements
+ *
+ * Return: Pointer to allocated memory or normal process termination
+ * with a status value of 98
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-		void *p;
+	void *p;
 
 
-			if (nmemb == 0 || size == 0)
-						return (0);
 
-				p = malloc(nmemb * size);
-					if (p == 0)
-								return (0);
-						_memset(p, 0, size * nmemb);
+	if (nmemb == 0 || size == 0)
+		return (0);
 
-							return (p);
+	p = malloc(nmemb * size);
+	if (p == 0)
+		return (0);
+	_memset(p, 0, size * nmemb);
+
+	return (p);
 }
