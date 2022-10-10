aa4f2ab8f5c732b0311d958b666be807e62816ab
yismaw17 <yismawmulaw@yahoo.com>
2022-10-10 04:36:49 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,7 @@
-#ifndef STRUCTS
-#define STRUCTS
+#ifndef DOG_H
+#define DOG_H
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * struct dog - description for a pet
  * @name: pet name
@@ -8,18 +10,16 @@
  *
  * Description: Longer description
  */
-typedef struct dog
+
+struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-}dog_t;
-#endif
-#ifndef _FUNCTIONS_H
-#define _FUNCTIONS_H
-int _putchar(char c);
+char *name;
+float age;
+char *owner;
+};
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
