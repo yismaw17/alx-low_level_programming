db0b86a11b0e29f25c9070ced31c514ea37365c6
yismaw17 <yismawmulaw@yahoo.com>
2022-10-14 04:05:56 -0700

    task 0

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,24 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
-
 /**
- * sum_them_all - return sum when given unknown amount of arguments
- * @n: integers to add
- * Return: sum
+ * sum_them_all -  the sum of all its parameters
+ * @n: input
+ * Return: void
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int sum = 0, i;
-
-	va_list ptr;
-
+	va_list total;
+	int sum = 0;
+	unsigned int x;
 	if (n == 0)
+	{
 		return (0);
-	va_start(ptr, n);
-
-	for (i = 0; i < n; i++)
-		sum += va_arg(ptr, int);
-	va_end(ptr);
-
+	}
+	va_start(total, n);
+	for (x = 0; x < n; x++)
+	{
+		sum = sum + va_arg(total, int);
+	}
+	va_end(total);
 	return (sum);
 }
