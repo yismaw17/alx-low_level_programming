18d0cad9be3e1e69f08e390a86e2a88c7dfbed73
yismaw17 <yismawmulaw@yahoo.com>
2022-10-14 04:09:08 -0700

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,14 +1,10 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int _strlen(const char * const s);
 
-#endif /* VARIADIC_FUNCTIONS_H */
+#endif
