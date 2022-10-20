e0da164146b82863de01b3dfacb1cefee6e7ca34
yismaw17 <yismawmulaw@yahoo.com>
2022-10-20 04:27:15 -0700

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,51 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
- *  * add_node_end - Add new node in the end of the linked list.
- *   * @head: head of the linked list.
- *    * @str: string to add.
- *     *
- *      * Return: pointer of the new node added to linked list.
- *       */
+ * len - Calculates the length of a string.
+ * @str: Constant string
+ * Return: The length of the string
+ */
 
-list_t *add_node_end(list_t **head, const char *str)
+int len(const char *str)
 {
-		list_t *new;
-			list_t *temp;
+	int count;
 
-				new = malloc(sizeof(list_t));
-					if (!new)
-								return (NULL);
-						temp = *head;
-							new->str = strdup(str);
-								new->len = _strlen(str);
-									new->next = NULL;
-										if (*head == NULL)
-												{
-															*head = new;
-																	return (new);
-																		}
-											while (temp->next != NULL)
-														temp = temp->next;
-												temp->next = new;
-													return (new);
+	if (str == NULL)
+				return (0);
+		for (count = 0; str[count] != '\0'; count++)
+					;
+			return (count);
 }
 
 /**
- *  * _strlen - Calculate lenght of a string.
- *   * @s: string.
- *    *
- *     * Return: lenght of the string s.
- *      */
+ * add_node_end - Function that adds a new node at the end of a list_t list.
+ *
+ * @head: This is the input linked list
+ * @str: This is the string take in my linked list.
+ *
+ * Return: the address of the new element, or NULL if it failed.
+ */
 
-int _strlen(const char *s)
+list_t *add_node_end(list_t **head, const char *str)
 {
-		int idx = 0;
+		list_t *new_node = (list_t *) malloc(sizeof(list_t));
+			list_t *last = *head;
 
-			for (; s[idx]; idx++)
-					;
-				return (idx);
+				if (new_node == NULL || str == NULL)
+						{
+									free(new_node);
+											return NULL;
+												}
+					new_node->str = strdup(str);
+						new_node->len = len(str);
+							new_node->next = NULL;
+								if (*head == NULL)
+										{
+													*head = new_node;
+															return (*head);
+																}
+									while (last->next != NULL)
+												last = last->next;
+										last->next = new_node;
+											return (*head);
 }
