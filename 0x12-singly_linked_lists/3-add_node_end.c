bcb7dfe7aaec56ae918432563874cccd0473eabd
yismaw17 <yismawmulaw@yahoo.com>
2022-10-20 05:31:46 -0700

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,10 +11,10 @@ int len(const char *str)
 	int count;
 
 	if (str == NULL)
-				return (0);
-		for (count = 0; str[count] != '\0'; count++)
-					;
-			return (count);
+		return (0);
+	for (count = 0; str[count] != '\0'; count++)
+		;
+	return (count);
 }
 
 /**
@@ -28,24 +28,25 @@ int len(const char *str)
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-		list_t *new_node = (list_t *) malloc(sizeof(list_t));
-			list_t *last = *head;
+	list_t *new_node = (list_t *) malloc(sizeof(list_t));
+	list_t *last = *head;
 
-				if (new_node == NULL || str == NULL)
-						{
-									free(new_node);
-											return NULL;
-												}
-					new_node->str = strdup(str);
-						new_node->len = len(str);
-							new_node->next = NULL;
-								if (*head == NULL)
-										{
-													*head = new_node;
-															return (*head);
-																}
-									while (last->next != NULL)
-												last = last->next;
-										last->next = new_node;
-											return (*head);
+	if (new_node == NULL || str == NULL)
+	{
+		free(new_node);
+		return NULL;
+	}
+	new_node->str = strdup(str);
+	new_node->len = len(str);
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new_node;
+	return (*head);
 }
+
