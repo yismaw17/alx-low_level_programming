ee76fad521b8ac6b85fffbebed37833324adc4e2
yismaw17 <yismawmulaw@yahoo.com>
2022-10-25 07:08:04 -0700

    task 14

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -19,7 +19,7 @@ size_t looped_listint_count(listint_t *head)
 
 	if (head == NULL || head->next == NULL)
 		return (0);
-	
+
 	tortoise = head->next;
 	hare = (head->next)->next;
 
@@ -35,23 +35,35 @@ size_t looped_listint_count(listint_t *head)
 				hare = hare->next;
 			}
 
-			return (0);
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+
+			return (nodes);
 		}
 
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+
+	return (0);
+}
 
 
-		/**
-		 * free_listint_safe - Frees a listint_t list safely (ie.
-		 * can free lists containing loops)
-		 *
-		 *  @h: A pointer to the address of
-		 *  the head of the listint_t list.
-		 *
-		 *  Return: The size of the list that was freed.
-		 *
-		 *  Description: The function sets the head to NULL.
-		 */
-		size_t free_listint_safe(listint_t **h)
+/*
+ * free_listint_safe - Frees a listint_t list safely (ie.
+ * can free lists containing loops)
+ * @h: A pointer to the address of
+ * the head of the listint_t list.
+ *
+ * Return: The size of the list that was free
+ *
+ * Description: The function sets the head to NULL.
+ */
+size_t free_listint_safe(listint_t **h)
 		{
 			listint_t *tmp;
 			size_t nodes, index;
@@ -83,5 +95,4 @@ size_t looped_listint_count(listint_t *head)
 			h = NULL;
 
 			return (nodes);
-		}
-			
+		}			
