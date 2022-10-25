97fd32a74b85dd330053d3a00fdfd0eb1ca6b4b8
yismaw17 <yismawmulaw@yahoo.com>
2022-10-25 06:08:29 -0700

    task 12

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -34,66 +34,54 @@ size_t looped_listint_count(listint_t *head)
 				tortoise = tortoise->next;
 				hare = hare->next;
 			}
-			tortoise = tortoise->next;
-						while (tortoise != hare)
-										{
-															nodes++;
-																			tortoise = tortoise->next;
-																						}
 
-									return (nodes);
-											}
-
-				tortoise = tortoise->next;
-						hare = (hare->next)->next;
-							}
-
-		return (0);
-}
+			return (0);
+		}
 
 
 
-/**
- *  * free_listint_safe - Frees a listint_t list safely (ie.
- *   * can free lists containing loops)
- *    *
- *     * @h: A pointer to the address of
- *      * the head of the listint_t list.
- *       *
- *        * Return: The size of the list that was freed.
- *         *
- *          * Description: The function sets the head to NULL.
- *           */
-size_t free_listint_safe(listint_t **h)
-{
-		listint_t *tmp;
+		/**
+		 * free_listint_safe - Frees a listint_t list safely (ie.
+		 * can free lists containing loops)
+		 *
+		 *  @h: A pointer to the address of
+		 *  the head of the listint_t list.
+		 *
+		 *  Return: The size of the list that was freed.
+		 *
+		 *  Description: The function sets the head to NULL.
+		 */
+		size_t free_listint_safe(listint_t **h)
+		{
+			listint_t *tmp;
 			size_t nodes, index;
 
-				nodes = looped_listint_count(*h);
+			nodes = looped_listint_count(*h);
 
-					if (nodes == 0)
-							{
-										for (; h != NULL && *h != NULL; nodes++)
-													{
-																	tmp = (*h)->next;
-																				free(*h);
-																							*h = tmp;
-																									}
-											}
+			if (nodes == 0)
+			{
+				for (; h != NULL && *h != NULL; nodes++)
+				{
+					tmp = (*h)->next;
+					free(*h);
+					*h = tmp;
+				}
+			}
 
-						else
-								{
-											for (index = 0; index < nodes; index++)
-														{
-																		tmp = (*h)->next;
-																					free(*h);
-																								*h = tmp;
-																										}
+			else
+			{
+				for (index = 0; index < nodes; index++)
+				{
+					tmp = (*h)->next;
+					free(*h);
+					*h = tmp;
+				}
 
-													*h = NULL;
-														}
+				*h = NULL;
+			}
 
-							h = NULL;
+			h = NULL;
 
-								return (nodes);
-}
+			return (nodes);
+		}
+			
